91dd09f39b9c8380002068b3a8a3e711340523db
BryanB-Dev <perso703@pm.me>
2023-12-18 18:41:00 +0100

    Refactor Introduction component and add Text component

--- src/components/Introduction/index.jsx
+++ src/components/Introduction/index.jsx
@@ -1,19 +1,12 @@
+import Text from "../Text";
 import Title from "../Title";
 import "./style.scss";
 
 export default function Introduction() {
     return (
         <div className="introduction">
-            <Title text="LET ME %INTRODUCE% MYSELF" size={2.5}/>
-            <p className="introduction__text">
-                I fell in love with programming and I have at least learnt something, I think… 🤷‍♂️<br />
-                <br />
-                I am fluent in classics like <span className="primaryColor">C++, Javascript and Go.</span><br />
-                <br />
-                My field of Interest's are building new  Web Technologies and Products and also in areas related to Blockchain.<br />
-                <br />
-                Whenever possible, I also apply my passion for developing products with Node.js and Modern Javascript Library and Frameworks  like React.js and Next.js
-            </p>
+            <Title text="LET ME %INTRODUCE% MYSELF" size={2.5} />
+            <Text text={["I fell in love with programming and I have at least learnt something, I think… 🤷‍♂️/n/nI am fluent in classics like %C++, Javascript and Go.%/n/nMy field of Interest's are building new  Web Technologies and Products and also in areas related to Blockchain./n/nWhenever possible, I also apply my passion for developing products with Node.js and Modern Javascript Library and Frameworks  like React.js and Next.js"]} size={1.2} />
         </div>
     )
 }
