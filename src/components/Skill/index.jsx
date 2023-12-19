5558443243956c10a3e3edf900b25d5d84c44f26
BryanB-Dev <perso703@pm.me>
2023-12-19 03:16:12 +0100

    Add react-icons package and display JavaScript skill

---
+++ src/components/Skill/index.jsx
@@ -0,0 +1,10 @@
+import "./style.scss";
+
+export default function Skill({ children }) {
+    
+    return (
+        <div className="skill">
+            {children}
+        </div>
+    );
+}
--- src/pages/skills/index.jsx
+++ src/pages/skills/index.jsx
@@ -1,7 +1,13 @@
+import Skill from "../../components/Skill";
+import { DiJavascript1 } from "react-icons/di";
+
 export default function Skills() {
 
     return (
         <>
+        <Skill>
+            <DiJavascript1 />
+        </Skill>
         </>
     );
 }
