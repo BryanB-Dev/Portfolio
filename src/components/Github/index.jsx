a0e2a18489b4c59c6be430cce91f66a3f66bafe2
BryanB-Dev <perso703@pm.me>
2023-12-19 04:23:54 +0100

    Add Github Component and react-github-calendar package and update dependencies

---
+++ src/components/Github/index.jsx
@@ -0,0 +1,24 @@
+import "./style.scss";
+import Title from "../Title";
+import GitHubCalendar from "react-github-calendar";
+
+export default function Github({ user }) {
+
+  const minimalTheme = {
+    light: ['#444', 'hsla(272, 85%, 74%, 0.44)', 'hsla(272, 85%, 74%, 0.6)', 'hsla(272, 85%, 74%, 0.76)', 'hsla(272, 85%, 74%, 0.92)'],
+    dark: ['#FFF', 'hsla(272, 85%, 74%, 0.44)', 'hsla(272, 85%, 74%, 0.6)', 'hsla(272, 85%, 74%, 0.76)', 'hsla(272, 85%, 74%, 0.92)']
+  };
+
+  return (
+    <div className="github">
+      <Title text="Contributions %Github%" size={2.3} />
+      <GitHubCalendar
+        username={user}
+        blockSize={15}
+        blockMargin={5}
+        theme={minimalTheme}
+        fontSize={16}
+      />
+    </div>
+  );
+}
--- src/pages/skills/index.jsx
+++ src/pages/skills/index.jsx
@@ -1,17 +1,21 @@
 import { DiCss3, DiDebian, DiGit, DiHtml5, DiJavascript1, DiMongodb, DiMysql, DiNodejs, DiPhp, DiPython, DiReact, DiSass, DiUbuntu, DiWindows } from "react-icons/di";
 import { SiExpress, SiKalilinux, SiMariadbfoundation, SiPostman, SiSqlite, SiVisualstudiocode } from "react-icons/si";
 import SkillContainer from "../../components/SkillContainer";
+import Github from "../../components/Github";
 
 export default function Skills() {
 
     return (
         <>
-        <SkillContainer datas={{title:"Systèmes %D'exploitation%", skills: [<DiWindows/>, <DiDebian/>, <DiUbuntu/>, <SiKalilinux/>]}}/>
-        <SkillContainer datas={{title:"Outils de %Développement%", skills: [<SiVisualstudiocode/>, <DiGit/>, <SiPostman/>]}}/>
-        <SkillContainer datas={{title:"Bases de %Données%", skills: [<DiMysql/>, <SiMariadbfoundation/>, <DiMongodb/>, <SiSqlite/>]}}/>
-        <SkillContainer datas={{title:"Langage %Back-end%", skills: [<DiPython/>, <DiPhp/>, <DiNodejs/>, <SiExpress/>]}}/>
-        <SkillContainer datas={{title:"Langage %Front-end%", skills: [<DiHtml5/>, <DiCss3/>, <DiJavascript1/>]}}/>
-        <SkillContainer datas={{title:"Préprocesseurs et %Framework%", skills: [<DiSass/>, <DiReact/>]}}/>
+        <div className="skillSection">
+            <SkillContainer datas={{title:"Systèmes %D'exploitation%", skills: [<DiWindows/>, <DiDebian/>, <DiUbuntu/>, <SiKalilinux/>]}}/>
+            <SkillContainer datas={{title:"Outils de %Développement%", skills: [<SiVisualstudiocode/>, <DiGit/>, <SiPostman/>]}}/>
+            <SkillContainer datas={{title:"Bases de %Données%", skills: [<DiMysql/>, <SiMariadbfoundation/>, <DiMongodb/>, <SiSqlite/>]}}/>
+            <SkillContainer datas={{title:"Langage %Back-end%", skills: [<DiPython/>, <DiPhp/>, <DiNodejs/>, <SiExpress/>]}}/>
+            <SkillContainer datas={{title:"Langage %Front-end%", skills: [<DiHtml5/>, <DiCss3/>, <DiJavascript1/>]}}/>
+            <SkillContainer datas={{title:"Préprocesseurs et %Framework%", skills: [<DiSass/>, <DiReact/>]}}/>
+        </div>
+        <Github user={"BryanB-Dev"}/>
         </>
     );
 }
