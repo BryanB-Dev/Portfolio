c82f3d7a4b0c2a410d34d63aacd42052c6946428
BryanB-Dev <perso703@pm.me>
2023-12-19 05:49:07 +0100

    Remove unused imports in Profile component

--- src/pages/profile/index.jsx
+++ src/pages/profile/index.jsx
@@ -1,7 +1,4 @@
-import ProfilePng from "../../assets/profile.png";
 import "./style.scss";
-import Title from "../../components/Title";
-import Text from "../../components/Text";
 import About from "../../components/About";
 
 export default function Profile() {
