385650725be0bd2b7a0962aed4c20dcd48c999a9
BryanB-Dev <perso703@pm.me>
2023-12-19 06:54:22 +0100

    Update About component to use data from profile.json

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -3,15 +3,15 @@ import Title from "../Title";
 import Text from "../Text";
 import "./style.scss";
 
-export default function About() {
+export default function About({ datas }) {
 
     return (
         <>
             <div className="aboutSection">
                 <div className="aboutSection__text">
-                    <Title text="Know Who %I'M%" size={2.1} />
+                    <Title text={datas.title} size={2.1} />
                     <div className="aboutSection__description">
-                    <Text text={["Hi Everyone, I am %Soumyajit Behera% from %Bhubaneswar, India.%/nI am currently employed as a software developer at Juspay. I have completed Integrated MSc (IMSc) in Maths and Computing at BIT Mesra./n/nApart from coding, some other activities that I love to do!", "Playing Games/nWriting Tech Blogs/nTravelling"]} size={1.25} />
+                        <Text text={datas.text} size={1.25} />
                     </div>
                 </div>
                 <img className="aboutSection__img" src={ProfilePng} alt="" />
--- src/pages/profile/index.jsx
+++ src/pages/profile/index.jsx
@@ -1,11 +1,12 @@
 import "./style.scss";
 import About from "../../components/About";
+import ProfileData from "../../data/profile.json";
 
 export default function Profile() {
 
     return (
         <>
-            <About />
+            <About datas={ProfileData} />
         </>
     );
 }
